475917a3b4ed78a9b12d3905fc95b2c1783f40eb
Samuel Maiko <sammymaiko721@gmail.com>
2023-08-18 21:45:29 +0300

    Python code attempt

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self,title, page_count):
+        self.title=title
+        self.page_count=page_count
+
+    def get_page(self):
+        return self.page_count
+    def set_page(self,page):
+        if type(page) !=int:
+            print("page_count must be an integer")
+        else:
+            self.page_count=page
+    page_count=property(get_page, set_page,)
     
-        
+    def turn_page(self):
+        self.page_count+=1
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand=brand
+        self.size=size
+
+    def get_size(self):
+        return self.size
+    def set_size(self,size):
+        if type(size) !=int:
+            print("size must be an integer")
+        else:
+            self.size=size
+    page_count=property(get_size, set_size,)
+
+    def repair(self):
+        print("The shoe has been repaired")
+        self.condition='New'
